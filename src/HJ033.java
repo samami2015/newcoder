916d459c811d1056d0f5331a006cbfd2929d9645
wangsen <22232647@qq.com>
2021-07-23 01:14:59 +0800

    newcoder 1.增加题目描述 2.优化部分代码

--- src/HJ033.java
+++ src/HJ033.java
@@ -2,6 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 【整数与IP地址间的转换】
+ * 描述
+ * 原理：ip地址的每段可以看成是一个0-255的整数，把每段拆分成一个二进制形式组合起来，然后把这个二进制数转变成
+ * 一个长整数。
+ * 举例：一个ip地址为10.0.3.193
+ * 每段数字             相对应的二进制数
+ * 10                   00001010
+ * 0                    00000000
+ * 3                    00000011
+ * 193                  11000001
+ * 组合起来即为：00001010 00000000 00000011 11000001,转换为10进制数就是：167773121，即该IP地址转换后的数字就是它了。
+ * 本题含有多组输入用例，每组用例需要你将一个ip地址转换为整数、将一个整数转换为ip地址。
+ * 输入描述：
+ * 输入
+ * 1 输入IP地址
+ * 2 输入10进制型的IP地址
+ * <p>
+ * 输出描述：
+ * 输出
+ * 1 输出转换成10进制的IP地址
+ * 2 输出转换后的IP地址
+ * <p>
+ * 示例1
+ * 输入：
+ * 10.0.3.193
+ * 167969729
+ * 输出：
+ * 167773121
+ * 10.3.3.193
+ */
 public class HJ033 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/HJ035.java
+++ src/HJ035.java
@@ -1,5 +1,32 @@
 import java.io.*;
 
+/**
+ * 【蛇形矩阵】
+ * 描述
+ * 蛇形矩阵是由1开始的自然数依次排列成的一个矩阵上三角形。
+ * 例如，当输入5时，应该输出的三角形为：
+ * 1 3 6 10 15
+ * 2 5 9 14
+ * 4 8 13
+ * 7 12
+ * 11
+ * <p>
+ * 请注意本题含有多组样例输入。
+ * <p>
+ * 输入描述：
+ * 输入正整数N（N不大于100）
+ * <p>
+ * 输出描述：
+ * 输出一个N行的蛇形矩阵。
+ * 示例1
+ * 输入：
+ * 4
+ * 输出：
+ * 1 3 6 10
+ * 2 5 9
+ * 4 8
+ * 7
+ */
 public class HJ035 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
