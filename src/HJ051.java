bc33254bf3f4cedbd5fcf84b606ddc0dcf098889
wangsen <22232647@qq.com>
2021-07-25 00:56:24 +0800

    newcoder 1.增加题目描述 2.优化部分代码

--- src/HJ051.java
+++ src/HJ051.java
@@ -1,6 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 【输出单向链表中倒数第k个结点】
+ * 描述
+ * 输入一个单向链表，输出该链表中倒数第k个结点，链表的倒数第1个结点为链表的尾指针。
+ * 链表结点定义如下：
+ * struct ListNode
+ * {
+ * int       m_nKey;
+ * ListNode* m_pNext;
+ * };
+ * 正常返回倒数第k个结点指针，异常返回空指针
+ * 本题有多组样例输入。
+ * 输入描述：
+ * 输入说明
+ * 1 输入链表结点个数
+ * 2 输入链表的值
+ * 3 输入k的值
+ * 输出描述：
+ * 输出一个整数
+ * 示例1
+ * 输入：
+ * 8
+ * 1 2 3 4 5 6 7 8
+ * 4
+ * 输出：
+ * 5
+ */
 public class HJ051 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/HJ055.java
+++ src/HJ055.java
@@ -2,6 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 【挑7】
+ * 描述
+ * 输出7有关数字的个数，包括7的倍数，还有包含7的数字（如17，27，37...70，71，72，73...）的个数（一组测试用例里可能有多组数据，请注意处理）
+ * 输入描述：
+ * 一个正整数N。(N不大于30000)
+ * 输出描述：
+ * 不大于N的与7有关的数字个数，例如输入20，与7有关的数字包括7,14,17.
+ * 示例1
+ * 输入：
+ * 20
+ * 10
+ * 输出：
+ * 3
+ * 1
+ */
 public class HJ055 {
     public static boolean iscontain7(int num) {
         int count = 0;
